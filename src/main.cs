944aaf9b5c5fc4cb9ecae8fd70bcadaf19201236
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-03-20 23:43:58 +0100

    added some more stuff

--- src/main.cs
+++ src/main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Windows.UI.Xaml;
 
 namespace myApp
 {
@@ -11,6 +12,31 @@ namespace myApp
             result = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("You entered: " + result);
             Console.ReadLine();
+
+            string[] names = { "charles", "sabbath", "destiny", "cassey" };
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            names[4] = "james";
+
+            if (names[4] != "james")
+            {
+                Console.WriteLine("Name not Found");
+            }
+            else
+            {
+                Console.WriteLine("James is found");
+            }
+
+
+            void is_on()
+            {
+                if (1 > 0)
+                {
+                    Console.WriteLine("Omor");
+                }
+            }
 
         }
     }
