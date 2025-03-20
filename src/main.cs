b9eac8eb5ca81ccddc669664e1e6fb7e2fc37eac
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-03-20 23:22:31 +0100

    Working app

--- src/main.cs
+++ src/main.cs
@@ -7,6 +7,11 @@ namespace myApp
         public static void main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int result;
+            result = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + result);
+            Console.ReadLine();
+
         }
     }
 }
